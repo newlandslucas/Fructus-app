3ce7b1cc187370c4fd0dca6b37f2e1cdf3a7db3c
newlandslucas <lucasnewlands.m@gmail.com>
2022-03-29 14:46:21 -0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Lucas Newlands on 29/03/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
