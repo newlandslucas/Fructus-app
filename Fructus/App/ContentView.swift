5abde1b93c8a8054a6c2093605fb98a9dff83274
newlandslucas <lucasnewlands.m@gmail.com>
2022-04-05 21:19:52 -0300

    development: FruitCardView

--- Fructus/App/ContentView.swift
+++ Fructus/App/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color("ColorBlueberryLight")
+                .ignoresSafeArea(.all)
+            Text("Lucas Newlands")
+                .padding()
+            
+            Spacer()
+        }
+        
+        
+        
+        
     }
 }
 
---
+++ Fructus/FruiCardView.swift
@@ -0,0 +1,57 @@
+//
+//  FruiCardView.swift
+//  Fructus
+//
+//  Created by Lucas Newlands on 05/04/22.
+//
+
+import SwiftUI
+
+struct FruiCardView: View {
+    
+    //Mark - Properties
+    
+    //Mark - Body
+    
+    
+    var body: some View {
+        ZStack {
+            VStack(spacing: 20) {
+                //Frui: image
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+                
+                //Fruit: title
+                Text("Blueberry")
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+                
+                //Fruit: Headline
+                Text("Blueberry are sweet, nutricious and wildly popular fruit all over the world.")
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: 480)
+                
+                //Button: Start
+                
+            }//VStack
+        } // ZStack
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
+    }
+}
+
+//Mark - Preview
+
+struct FruiCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruiCardView()
+            .previewLayout(.fixed(width: 320, height: 640))
+    }
+}
