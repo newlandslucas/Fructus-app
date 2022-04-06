00b715d8350ad9ebdd4117bbbb962d98449cb207
newlandslucas <lucasnewlands.m@gmail.com>
2022-04-05 21:26:43 -0300

    feat: StartButton component

---
+++ Fructus/StartButtonView.swift
@@ -0,0 +1,41 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by Lucas Newlands on 05/04/22.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    //Mark - Properties
+    
+    //Mark - Body
+    var body: some View {
+        Button(action: {
+            print("Exit the onboarding")
+        }) {
+            HStack(spacing: 8) {
+                Text("Start")
+                
+                Image(systemName: "arrow.right.circle")
+                    .imageScale(.large)
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 10)
+            .background(Capsule().strokeBorder(Color.white, lineWidth: 1.25))
+        }
+        .accentColor(Color.white)
+        
+    }
+}
+
+// Mark - Preview
+
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+    }
+}
